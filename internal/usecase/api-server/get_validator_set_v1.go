1c46e47ad02affc596dff4e32dc3496a223b138b
oxsteins <oxsteins@gmail.com>
2025-10-01 18:09:45 +0530

    fix: refactor error handling
    Signed-off-by: oxsteins <oxsteins@gmail.com>

--- internal/usecase/api-server/get_validator_set_v1.go
+++ internal/usecase/api-server/get_validator_set_v1.go
@@ -39,18 +39,16 @@ func (h *grpcHandler) GetValidatorSet(ctx context.Context, req *apiv1.GetValidat
 
 // getValidatorSetForEpoch retrieves validator set for a given epoch, either from repo or by deriving it
 func (h *grpcHandler) getValidatorSetForEpoch(ctx context.Context, epochRequested entity.Epoch) (entity.ValidatorSet, error) {
-	var validatorSet entity.ValidatorSet
-
 	validatorSet, err := h.cfg.Repo.GetValidatorSetByEpoch(ctx, epochRequested)
 	if err == nil {
 		return validatorSet, nil
-	} else if !errors.Is(err, entity.ErrEntityNotFound) {
-		return entity.ValidatorSet{}, errors.Errorf("failed to get validator set for epoch %d: %v", epochRequested, err)
 	}
+
 	if errors.Is(err, entity.ErrEntityNotFound) {
 		return entity.ValidatorSet{}, errors.Errorf("validator set for epoch %d not found", epochRequested)
 	}
-	return validatorSet, nil
+
+	return entity.ValidatorSet{}, errors.Errorf("failed to get validator set for epoch %d: %w", epochRequested, err)
 }
 
 func convertValidatorSetToPB(valSet entity.ValidatorSet) *apiv1.GetValidatorSetResponse {
